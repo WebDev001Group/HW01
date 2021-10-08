46e5120c4d42ebd226524dbd29848786b44e7c19
dorrin-sot <dorrinsotoudeh123@gmail.com>
2021-10-08 16:07:04 +0400

    added redis to post and get methods

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+const redis = require("redis");
+const client = redis.createClient();
+
 var app = express();
 
-var db = new Map()
 var crypto = require('crypto')
 app.post('/node/sha256', function (req, res) {
     // todo save the sha256 in db
@@ -12,10 +14,10 @@ app.post('/node/sha256', function (req, res) {
     } else {
         sha256d = crypto.createHash('sha256').update(str).digest('base64')
 
-        db.set(sha256d, str)
-        console.log('hashed ' + str)
-
-        res.json({[str]: sha256d})
+        client.set(sha256d, str, function (err) {
+            redis.print();
+            res.json({[str]: sha256d})
+        });
     }
 })
 
@@ -23,15 +25,20 @@ app.get('/node/sha256', function (req, res) {
     // todo get un-sha256 from db
     let sha256dInReq = req.headers.sha256d
 
-    let str = db.get(sha256dInReq)
-    if (str !== undefined) {
-        res.json({[sha256dInReq]: str})
-    } else {
-        console.error('no str found for sha256d')
-        res.status(404).send({message: 'no str found for this sha256d'})
-    }
+    client.get(sha256dInReq, function (err, str) {
+        if (str !== null) {
+            res.json({[sha256dInReq]: str})
+        } else {
+            console.error('no str found for sha256d')
+            res.status(404).send({message: 'no str found for this sha256d'})
+        }
+    })
 })
 
 var server = app.listen(8081, () => {
     console.log("Example app listening at PORT=%d", server.address().port)
 })
+
+client.on("error", function (error) {
+    console.error(error);
+});
