558a8400606d52859c3134ea7de21abf26d570fe
dorrin-sot <dorrinsotoudeh123@gmail.com>
2021-10-08 15:37:30 +0400

    added the get method

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var sha256d = ''
+var db = new Map()
 var crypto = require('crypto')
 app.post('/node/sha256', function (req, res) {
     // todo save the sha256 in db
@@ -11,13 +11,25 @@ app.post('/node/sha256', function (req, res) {
         res.status(400).send({message: 'str should be at least 8 characters'})
     } else {
         sha256d = crypto.createHash('sha256').update(str).digest('base64')
-        console.log('sha256(' + str + ') = ' + sha256d)
-        res.json({result: sha256d})
+
+        db.set(sha256d, str)
+        console.log('hashed ' + str)
+
+        res.json({[str]: sha256d})
     }
 })
 
 app.get('/node/sha256', function (req, res) {
     // todo get un-sha256 from db
+    let sha256dInReq = req.headers.sha256d
+
+    let str = db.get(sha256dInReq)
+    if (str !== undefined) {
+        res.json({[sha256dInReq]: str})
+    } else {
+        console.error('no str found for sha256d')
+        res.status(404).send({message: 'no str found for this sha256d'})
+    }
 })
 
 var server = app.listen(8081, () => {
